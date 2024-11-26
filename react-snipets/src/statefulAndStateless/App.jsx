6d4353205b494abd3666ae521d49f17c62beb86a
Albert Osei Frimpong <alotab@yahoo.com>
2024-11-26 05:57:14 -0800

    add useReducer hook

---
+++ react-snipets/src/statefulAndStateless/App.jsx
@@ -0,0 +1,22 @@
+
+
+
+import React, { useState } from 'react'
+
+const App = () => {
+    const [date] = useState(new Date())
+
+  return (
+    <div>
+        <Child message={date.toLocaleDateString()}/>
+    </div>
+  )
+}
+
+export default App
+
+
+// This is an example of the reationship between statefull and stateless components
+// Since child component cant store any state, they are called stateless component
+// All of it data comes in as props that is passed through by the parent component
+// Props in Child components are immutable and are only changed in the Parent component
